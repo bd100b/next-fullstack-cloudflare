d38c4296ade9ce2af5c213c2a49d667cdd0ab9f1
maghfoor-dev <maghfoor.dev@gmail.com>
2024-04-28 13:59:54 +0100

    initialised turso with drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+export default {
+  schema: "./app/db/schema.ts",
+  out: "./migrations",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN!,
+  },
+} satisfies Config;

ab0798c10a50d604aa89668ee79c5f8eb2ebab94
bd100b <55160143+bd100b@users.noreply.github.com>
2024-09-02 00:05:43 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import { getMessagesFromDatabase } from "@/actions/get-messages-from-database";
+// import { getMessagesFromDatabase } from "@/actions/get-messages-from-database";
 import MessageForm from "@/components/message-form";
 
 export const runtime = "edge";
 
 export default async function Home() {
-  const messages = await getMessagesFromDatabase();
+  // const messages = await getMessagesFromDatabase();
   return (
     <main className="max-w-2xl mx-auto p-10">
       <MessageForm />
       <div className="p-5">
         <ul>
-          {messages.map((message) => {
-            return <li key={message.id}>{message.message}</li>;
-          })}
+          <li>test 1</li>;
+          <li>test 2</li>;
         </ul>
       </div>
     </main>

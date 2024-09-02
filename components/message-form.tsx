17511f809a23d28f6814f73fd4e769c472508fb3
bd100b <bdanulchenko@outlook.com>
2024-09-02 18:45:38 +0300

    delete db files

--- components/message-form.tsx
+++ components/message-form.tsx
@@ -3,18 +3,11 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { addMessageToDatabase } from "@/actions/add-message-to-database";
 
 export default function MessageForm() {
   const [message, setMessage] = useState("");
 
-  async function handleAddMessageToDatabase() {
-    const response = await addMessageToDatabase(message);
 
-    if (response.ok) {
-      setMessage("");
-    }
-  }
   return (
     <div className="flex flex-col justify-center gap-3">
       <Input
@@ -22,7 +15,7 @@ export default function MessageForm() {
         value={message}
         onChange={(event) => setMessage(event.target.value)}
       />
-      <Button onClick={handleAddMessageToDatabase}>Submit</Button>
+      <Button>Submit</Button>
     </div>
   );
 }

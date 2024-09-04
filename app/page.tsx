66293ac3909fa41373d4be8f2c67f5aba9abbccb
bd100b <bdanulchenko@outlook.com>
2024-09-04 16:27:58 +0300

    added backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-// import { getMessagesFromDatabase } from "@/actions/get-messages-from-database";
 "use client";
+import { getMessagesFromDatabase } from "@/actions/get-messages-from-database";
 import MessageForm from "@/components/message-form";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
 export default async function Home() {
-  // const messages = await getMessagesFromDatabase();
+  const messages = await getMessagesFromDatabase();
   return (
     <main className="max-w-2xl mx-auto p-10">
       <MessageForm />
       <div className="p-5">
         <ul>
-          <li>test 1</li>;
-          <li>test 2</li>;
+          {messages.map((message) => {
+            return <li key={message.id}>{message.message}</li>;
+          })}
         </ul>
       </div>
     </main>
